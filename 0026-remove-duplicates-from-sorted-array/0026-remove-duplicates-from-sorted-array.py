c7ff84916b7c3be46186e27f256cc0864a22a3d9
RAJPUT CHIRAGSINGH ARJUNSINGH <66815672+RajputChirag27@users.noreply.github.com>
2026-07-04 14:20:14 +0530

    Time: 4 ms (23.49%), Space: 20.3 MB (96.96%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # Let's we have one index keeping track of the element
+        # we will check like that first set prev at zero index.
+        # and one pointer to track the index.
+        # Then we will check if the element change increment the pointer and  # change the prev element 
+        n =  len(nums)
+        if n == 1:
+            return n
+        cIdx = 1
+        
+        for i in range(1,n):
+            if nums[i-1] != nums[i]:
+                nums[cIdx] = nums[i]
+                cIdx = cIdx + 1
+        
+        return cIdx
