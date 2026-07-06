e6f981b7f7821a47a158013efdd411bf2fec5ea0
RAJPUT CHIRAGSINGH ARJUNSINGH <66815672+RajputChirag27@users.noreply.github.com>
2026-07-07 00:27:25 +0530

    Time: 0 ms (100%), Space: 19.3 MB (25.84%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        # twor strings haystack and needle are given
+        # I need to return the index of first occurence of needle in haystack,
+        # or return -1 if needle is not a part of haystack
+        # need to scan the loop two pointers needed and one to keep track of # the start other to scan the word needle if found return index and  # if not found or mismatch the whole word return -1
+        # two loops are needed to solve the problem
+        # 1 <= haystack.length, needle.length <= 104
+        # haystack and needle consist of only lowercase English characters.
+
+        hLen = len(haystack)
+        nLen = len(needle)
+        tLen = hLen - nLen + 1
+        for i in range(tLen):
+            if haystack[i: i+nLen] == needle:
+                return i
+
+        return -1
